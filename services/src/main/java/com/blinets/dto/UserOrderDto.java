d63c73c39869672318df1ad559b4742224d8959b
pabl0219 <pavel.blinets@netcracker.com>
2019-11-27 14:50:47 +0300

    stab UserOrder

---
+++ services/src/main/java/com/blinets/dto/UserOrderDto.java
@@ -0,0 +1,28 @@
+package com.blinets.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserOrderDto {
+
+  @JsonProperty("id")
+  private String idOrder;
+  @JsonProperty("price")
+  private Integer price;
+  @JsonProperty("status")
+  private String status;
+  @JsonProperty("full_time")
+  private LocalDate full_time;
+  @JsonProperty("start_date")
+  private LocalDate start_date;
+  @JsonProperty("end_date")
+  private LocalDate end_date;
+
+
+}
